0a63668af383089ff9cd0ae027bed94985cc8adb
Boris Nikolic <borisnikolic2302@gmail.com>
2024-02-17 15:26:51 +0100

    trpc fix

--- apps/client/app/trpc.ts
+++ apps/client/app/trpc.ts
@@ -1,9 +1,9 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import { AppRouter } from "@server/trpc/trpc.router";
-export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
-    }),
-  ],
-});
+// import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+// import { AppRouter } from "@server/trpc/trpc.router";
+// export const trpc = createTRPCProxyClient<AppRouter>({
+//   links: [
+//     httpBatchLink({
+//       url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
+//     }),
+//   ],
+// });
