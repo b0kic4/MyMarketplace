59c7889005e1b2115eedd3b3fd69ea39a6f6f647
Boris Nikolic <borisnikolic2302@gmail.com>
2024-02-19 14:53:36 +0100

    na minut do mora

--- apps/client/app/api/products/route.ts
+++ apps/client/app/api/products/route.ts
@@ -10,75 +10,77 @@ export async function POST(req: NextRequest) {
     const formData = await req.formData();
 
     console.log("form data: ", formData);
-
-    if (!formData || !formData.has("productData")) {
-      throw new Error("Form data is missing or does not contain productData");
+    if (!formData) {
+      throw new Error("From data is missing");
     }
-
-    const productData = JSON.parse(formData.get("productData") as string);
-    console.log("productData: ", productData);
-    if (
-      !productData ||
-      !productData.images ||
-      !Array.isArray(productData.images)
-    ) {
-      throw new Error("Invalid productData format");
-    }
-
-    const images = productData.images.map(
-      (image: { file: File; isLogo: boolean | string }) => {
-        return isFile(image.file) ? image.file : null;
-      }
+    // Filter files with names containing 'image_' and 'isLogo_'
+    const imageEntries = Array.from(formData.entries()).filter(
+      ([fieldName, fieldValue]) =>
+        (fieldName.startsWith("image_") || fieldName.startsWith("isLogo_")) &&
+        (isFile(fieldValue) || typeof fieldValue === "string")
     );
 
-    const isLogos = productData.images.map(
-      (image: { file: File; isLogo: boolean | string }) => {
-        return typeof image.isLogo === "boolean" || image.isLogo === "true";
-      }
-    );
+    if (!imageEntries) {
+      console.error("No image entries found: ", imageEntries);
+      return NextResponse.json({ error: "no image entries" });
+    }
 
-    console.log("isLogos: ", isLogos);
+    if (imageEntries.length === 0) {
+      return NextResponse.json({
+        success: false,
+        error: "No images or isLogo values found in the request",
+      });
+    }
+
+    // Separate image entries and isLogo entries
+    const isLogoEntries = imageEntries.filter(([fieldName]) =>
+      fieldName.startsWith("isLogo_")
+    );
+    const imageFileEntries = imageEntries.filter(([fieldName]) =>
+      fieldName.startsWith("image_")
+    );
+    console.log("LOGO ENTRIES: ", isLogoEntries);
+    console.log("IMAGE FILE ENTRIES: ", imageFileEntries);
+    // Convert the imageFileEntries and isLogoEntries to arrays
+    const images = imageFileEntries.map(
+      ([fieldName, fieldValue]) => fieldValue as File
+    );
+    const isLogos = isLogoEntries.map(
+      ([fieldName, fieldValue]) => fieldValue as string
+    );
     console.log("before if images: ", images);
-
-    if (!images || images.length === 0) {
+    console.log("is logos: ", isLogos);
+    if (!images) {
       console.log("no images: ", images);
+      console.log("no is logos: ", isLogos);
       return NextResponse.json({ error: "No images found" });
     }
-
     console.log("after if images: ", images);
-
+    console.log("after if is logos: ", isLogos);
     // Call the uploadMiddleware to handle image uploads
     const uploadResults = await uploadMiddleware(images);
 
     // Handle the results as needed
     console.log("Upload Results:", uploadResults);
-
-    if (!uploadResults || uploadResults.length !== images.length) {
+    if (!uploadResults) {
       console.log("upload results failed: ", uploadResults);
-      return NextResponse.json({ error: "Mismatch in upload results" });
+      return NextResponse.json({ error: "No upload results" });
     }
-
     // Create an array of isLogos and imageUrls
-    const isLogosAndImageUrls = isLogos.map((isLogo: any, index: any) => ({
+    const isLogosAndImageUrls = isLogos.map((isLogo, index) => ({
       isLogo,
       imageUrl: uploadResults[index]?.imageUrl,
     }));
-
     console.log("before if Is Logos and Image: ", isLogosAndImageUrls);
-
     if (!isLogosAndImageUrls) {
       console.log("No Logos and Image");
     }
-
     console.log("after if Is Logos and Image: ", isLogosAndImageUrls);
-
     // Respond to the frontend based on the upload results, isLogos, and imageUrls
     const success = uploadResults.every((result) => !!result.imageUrl);
     const imageUrls = uploadResults.map((result) => result.imageUrl);
     const errors = uploadResults.map((result) => result.error).filter(Boolean);
-
     console.log("image urls: ", imageUrls);
-
     if (success) {
       return NextResponse.json({
         success: true,
