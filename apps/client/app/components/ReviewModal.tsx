4af5a4eca8d31cf99ab2d38b1a6cbf3050f64883
Boris Nikolic <borisnikolic2302@gmail.com>
2024-03-08 18:26:42 +0100

    Cart fix and review modal configuration changed

--- apps/client/app/components/ReviewModal.tsx
+++ apps/client/app/components/ReviewModal.tsx
@@ -1,15 +1,18 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
+import { Textarea } from '@/components/ui/textarea';
 import { StarIcon } from '@radix-ui/react-icons';
 import { Label } from '@/components/ui/label';
-import { DialogTrigger } from '@radix-ui/react-dialog';
 
 const ReviewModal = ({ isOpen, onClose, onSubmit, productId, orderId }: any) => {
   const [rating, setRating] = useState<number>(0);
@@ -20,39 +23,50 @@ const ReviewModal = ({ isOpen, onClose, onSubmit, productId, orderId }: any) => 
     onClose(); // Close the modal after submitting
   };
 
+  const handleReviewChange = (event: any) => {
+    setReview(event.target.value);
+  };
+
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogTrigger>
-        <Button>
-          Submit your Review
-        </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add Review</Button>
       </DialogTrigger>
-      <DialogTitle>Review the product</DialogTitle>
-      <DialogContent className="space-y-4">
-        <div className="text-center">How would you rate this product?</div>
-        <div className="flex justify-center">
-          {[1, 2, 3, 4, 5].map((star) => (
-            <StarIcon
-              key={star}
-              onClick={() => setRating(star)}
-              className={`h-6 w-6 cursor-pointer ${star <= rating ? 'text-yellow-500' : 'text-gray-400'}`}
-              aria-label={`${star} Stars`}
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Review For Product</DialogTitle>
+          <DialogDescription>
+            Add review for product here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Add Comment
+            </Label>
+            <Textarea
+              id="review_comment"
+              value={review}
+              onChange={handleReviewChange}
+              placeholder="Very Nice Product..."
+              className="col-span-3"
             />
-          ))}
+          </div>
+          <div className="flex justify-center">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <StarIcon
+                key={star}
+                onClick={() => setRating(star)}
+                className={`h-6 w-6 cursor-pointer ${star <= rating ? 'text-yellow-500' : 'text-gray-400'}`}
+                aria-label={`${star} Stars`}
+              />
+            ))}
+          </div>
         </div>
-        <Label htmlFor='review'>Your review</Label>
-        <Input
-          id='review'
-          value={review}
-          onChange={(e) => setReview(e.target.value)}
-          placeholder="Write your review"
-          className="w-full"
-        />
+        <DialogFooter>
+          <Button type="submit" onSubmit={submitReview}>Save</Button>
+        </DialogFooter>
       </DialogContent>
-      <div className="flex justify-end space-x-2 p-4">
-        <Button onClick={onClose} variant="destructive">Cancel</Button>
-        <Button onClick={submitReview} variant="outline">Submit Review</Button>
-      </div>
     </Dialog>
   );
 };
